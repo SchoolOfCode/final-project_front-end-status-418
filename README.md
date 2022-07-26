ce33656fa7d206604696e5ecb20e39e2fede7655
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-07-26 16:15:32 +0100

    initialise project with cra

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# final-project_front-end
+# Status 418
+
+Our group, Status 418, are dedicated to solving the problem: “Sure, it’s easy to _start_ a new habit, but how do you make it stick?”
---
+++ rootine/src/components/App/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Rootine 🌳</h1>
+			</header>
+			<main>
+				<p>Welcome to our amazing habit-tracking app, Rootine 🌳</p>
+			</main>
+		</div>
+	);
+}
+
+export default App;
