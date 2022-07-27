aa9d933db8f4fede749ac877a258c0a38ea04bd0
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-07-27 14:08:37 +0100

    Rough positioning of header elements, added 2 sub divs to navbar

--- rootine/src/components/Navbar/Navbar.js
+++ rootine/src/components/Navbar/Navbar.js
@@ -1,16 +1,33 @@
 import "./Navbar.css";
 
-export default function Navbar () {
+export default function Navbar() {
     return (
-
-        <header>
-         <img className="header-logo"src="" alt="Rootine logo"/>
-         <img className="header-name"src="" alt="Rootine"/>
-         <a href="">Blogs</a>
-         <a href="">About</a>
-         <img className="hamburger-menu"src="" alt="hamburger-menu"/>
-
-         </header> 
-
-    )
+        <header className="header">
+            <div className="header-wrapper">
+                <div className="header-div-left">
+                    <img 
+                        className="header-logo" 
+                        src="" 
+                        alt="Rootine logo" />
+                    <img 
+                        className="header-name" 
+                        src="" 
+                        alt="Rootine" />
+             </div>
+                <div className="header-div-right">
+                    <a 
+                        className ="blogs"
+                        href="#">Blogs</a>
+                    <a 
+                        className="about"
+                        href="#">About</a>
+                    <img 
+                        className="hamburger-menu" 
+                        src="" 
+                        alt="hamburger-menu" />
+                </div>    
+                
+            </div>
+        </header>
+    );
 }
