7a28f11551de486430acc2d4c177c8b276adaae1
idman01 <idmman.abshir@gmail.com>
2022-07-27 11:05:08 +0100

    basic header element structure done

---
+++ rootine/src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import "./Navbar.css";
+
+export default function Navbar () {
+    return (
+
+        <header>
+         <img className="header-logo"src="" alt="Rootine logo"/>
+         <img className="header-name"src="" alt="Rootine"/>
+         <a href="">Blogs</a>
+         <a href="">About</a>
+         <img className="hamburger-menu"src="" alt="hamburger-menu"/>
+
+         </header> 
+
+    )
+}
