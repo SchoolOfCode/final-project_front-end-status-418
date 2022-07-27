000b5e1369a4d2cba811fd31426f1992bf02c03a
idman01 <idmman.abshir@gmail.com>
2022-07-27 16:07:35 +0100

    landing page.js done

---
+++ rootine/src/components/LandingPage/landingpage.js
@@ -0,0 +1,18 @@
+export default function landingPage() {
+  return (
+    <div className="landing-page-wrapper">
+      <div className="landing-page-left">
+        <h2> Tagline! Tagline! Tagline!</h2>
+        <img className="landing-page-image" src="" alt="landing-page-image" />
+      </div>
+      <div className="landing-page-right">
+        <p>
+          A few sentences of context on what the app offers, how it will help
+          you maintain your habits and thus you should definitely sign up to use
+          it
+        </p>
+        <button className="Login-button"> Login</button>
+      </div>
+    </div>
+  );
+}
