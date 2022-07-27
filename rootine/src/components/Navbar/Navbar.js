e1b9dc4fef45101ca7350bd689ab26b7aed1474f
mordecaioladimeji <cai.oladimeji@gmail.com>
2022-07-27 12:13:54 +0100

    seperating containers in header

--- rootine/src/components/Navbar/Navbar.js
+++ rootine/src/components/Navbar/Navbar.js
@@ -1,16 +1,17 @@
 import "./Navbar.css";
 
-export default function Navbar () {
-    return (
-
-        <header>
-         <img className="header-logo"src="" alt="Rootine logo"/>
-         <img className="header-name"src="" alt="Rootine"/>
-         <a href="">Blogs</a>
-         <a href="">About</a>
-         <img className="hamburger-menu"src="" alt="hamburger-menu"/>
-
-         </header> 
-
-    )
+export default function Navbar() {
+  return (
+    <header>
+      <div className="header-container-left">
+        <img className="header-logo" src="" alt="Rootine logo" />
+        <h1 className="Rootine-title">Rootine</h1>
+      </div>
+      <div className="header-container-right">
+        <a href="">Blog</a>
+        <a href="">About</a>
+        <img className="hamburger-menu" src="" alt="hamburger-menu" />
+      </div>
+    </header>
+  );
 }
