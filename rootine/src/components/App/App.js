ac05b92bc8f92623639c8ae8f6ea6caa43c18f65
Samantha Wu <samanthayywu@gmail.com>
2022-07-28 16:05:10 +0100

    deatilsPanel

--- rootine/src/components/App/App.js
+++ rootine/src/components/App/App.js
@@ -1,17 +1,19 @@
 import Footer from "../Footer/Footer";
 import "./App.css";
 import Navbar from "../Navbar/Navbar";
+import DetailsPanel from "../DetailsPanel/DetailsPanel";
 
 function App() {
-	return (
-		<div className="App">
-			<Navbar />
-			<main>
-				<p>Welcome to our amazing habit-tracking app, Rootine 🌳</p>
-			</main>
-			<Footer />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Navbar />
+      <main>
+        <p>Welcome to our amazing habit-tracking app, Rootine 🌳</p>
+        <DetailsPanel />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
