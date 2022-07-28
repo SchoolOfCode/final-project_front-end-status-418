ffbc4b574c1545b25b342c55374d8bee6862f498
Samantha Wu <samanthayywu@gmail.com>
2022-07-28 16:03:51 +0100

    box

---
+++ rootine/src/components/DetailsPanel/DetailsPanel.js
@@ -0,0 +1,17 @@
+import { Box } from "@chakra-ui/react";
+
+const DetailsPanel = () => {
+  return (
+    <Box
+      maxW="sm"
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      color="black"
+    >
+      This is the Box
+    </Box>
+  );
+};
+
+export default DetailsPanel;
