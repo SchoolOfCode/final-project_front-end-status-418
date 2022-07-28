74cb1bb48b6b75c4837871568ebf0c093833a414
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-07-28 10:50:05 +0100

    Chakra preliminary work

--- rootine/src/components/App/App.js
+++ rootine/src/components/App/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Navbar from "../Navbar/Navbar";
+import LandingPage from "../LandingPage/LandingPage.js";
 function App() {
 
 	return (
@@ -7,6 +8,7 @@ function App() {
 			<Navbar/>
 			<main>
 				<p>Welcome to our amazing habit-tracking app, Rootine 🌳</p>
+                <LandingPage/>
 			</main>
 		</div>
 	);
--- rootine/src/components/LandingPage/landingpage.js
+++ rootine/src/components/LandingPage/landingpage.js
@@ -1,7 +1,9 @@
-export default function landingPage() {
-  return (
-    <div className="landing-page-wrapper">
-      <div className="landing-page-left">
+import "./LandingPage.css";
+import { Flex, Center, Text } from "@chakra-ui/react";
+export default function LandingPage() {
+    return (
+        <div className="landing-page-wrapper">
+            {/* <div className="landing-page-left">
         <h2> Tagline! Tagline! Tagline!</h2>
         <img className="landing-page-image" src="" alt="landing-page-image" />
       </div>
@@ -12,7 +14,23 @@ export default function landingPage() {
           it
         </p>
         <button className="Login-button"> Login</button>
-      </div>
-    </div>
-  );
+      </div> */}
+            <Flex color="white" gap="5px">
+                <Center
+                    className="landing-page-left"
+                    flex="1 auto"
+                    bg="green.500"
+                >
+                    <Text>Box 1</Text>
+                </Center>
+                <Center
+                    className="landing-page-right"
+                    bg="blue.500"
+                    flex="1 auto"
+                >
+                    <Text>Box 2</Text>
+                </Center>
+            </Flex>
+        </div>
+    );
 }
