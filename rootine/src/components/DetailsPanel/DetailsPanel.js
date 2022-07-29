b7dee12df693daeab1e767fcb7e91ea20274b51e
mordecaioladimeji <cai.oladimeji@gmail.com>
2022-07-29 10:30:19 +0100

    rounding the corners of panel border & increasing height

--- rootine/src/components/DetailsPanel/DetailsPanel.js
+++ rootine/src/components/DetailsPanel/DetailsPanel.js
@@ -4,10 +4,14 @@ const DetailsPanel = () => {
   return (
     <Box
       maxW="sm"
-      borderWidth="1px"
-      borderRadius="lg"
+      height="60%"
+      borderWidth="3px"
+      borderRadius="4.5em"
       overflow="hidden"
       color="black"
+      boxShadow="md"
+      p="6"
+      bg="white"
     >
       This is the Box
     </Box>
