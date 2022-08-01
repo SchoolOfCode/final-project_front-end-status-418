a7ddc89d53bf007b1ba5553895e6160cfefde4f0
mordecaioladimeji <cai.oladimeji@gmail.com>
2022-08-01 15:30:57 +0100

    starting to make inputs editable

--- rootine/src/components/DetailsPanel/DetailsPanel.js
+++ rootine/src/components/DetailsPanel/DetailsPanel.js
@@ -1,6 +1,24 @@
-import { Box, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  VStack,
+  Heading,
+  Text,
+  Checkbox,
+  Editable,
+  EditableInput,
+  EditablePreview,
+  useEditableControls,
+  EditableTextarea,
+} from "@chakra-ui/react";
 
 const DetailsPanel = () => {
+  // // const {
+  // //   isEditing,
+  // //   getSubmitButtonProps,
+  // //   getCancelButtonProps,
+  // //   getEditButtonProps,
+  // // } = useEditableControls();
+
   return (
     <Box
       className="details-panel-parent"
@@ -17,39 +35,59 @@ const DetailsPanel = () => {
     >
       <VStack>
         <Box>
-          <h1 className="habit-name"> Walk the dog </h1>
+          <Editable
+            fontSize="3xl"
+            fontWeight="bold"
+            textAlign="center"
+            className="habit-name"
+            defaultValue="Walk the dog"
+          >
+            <EditablePreview />
+            <EditableInput />
+          </Editable>
         </Box>
 
         <Box className="current-streak">
           <i className="current-streak-icon"></i>
-          <p> You are currently on a ____ streak</p>
+          <Text> You are currently on a ____ streak</Text>
         </Box>
         <Box className="longest-streak">
           <i className="longest-streak-icon"></i>
-          <p> Your longest streak is ____ days</p>
+          <Text> Your longest streak is ____ days</Text>
         </Box>
 
         <Box className="description">
-          <h2> Description </h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua
-          </p>
+          <Heading size="sm"> Description </Heading>
+          <Editable
+            defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut 
+            labore et dolore magna aliqua"
+            textAlign="center"
+          >
+            <EditablePreview />
+            <EditableTextarea />
+          </Editable>
         </Box>
 
         <Box className="everyday-checkbox">
-          <p> Everyday </p>
-          <input type="checkbox"></input>
+          <Text> Everyday </Text>
+          <Checkbox></Checkbox>
         </Box>
 
         <Box className="frequency">
-          <p>
+          <Text>
             {" "}
             Frequency
-            <input type="textbox"></input>
-            Time
-            <input type="dropdown"></input>
-          </p>
+            <Editable defaultValue="1">
+              <EditablePreview />
+              <EditableInput />
+            </Editable>
+            Times
+            <select className="frequency-select">
+              <option>Weekly</option>
+              <option>Monthly</option>
+            </select>
+          </Text>
         </Box>
       </VStack>
 
