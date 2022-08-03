08ad16c025d92960a9470a07b5eab4f795e09dcf
idman01 <idmman.abshir@gmail.com>
2022-08-03 11:30:34 +0100

    save button added

--- rootine/src/components/DetailsPanel/DetailsPanel.js
+++ rootine/src/components/DetailsPanel/DetailsPanel.js
@@ -20,7 +20,8 @@ import {
   WrapItem,
   useColorModeValue,
   Tooltip,
-  Select
+  Select,
+  Button,
 } from "@chakra-ui/react";
 import { CheckIcon, CloseIcon } from "@chakra-ui/icons";
 
@@ -68,14 +69,14 @@ const DetailsPanel = () => {
             defaultValue="Walk the dog"
           >
             <Tooltip label="Click to edit">
-          <EditablePreview
-            py={2}
-            px={4}
-            _hover={{
-              background: useColorModeValue("gray.100", "gray.700")
-            }}
-          />
-        </Tooltip>
+              <EditablePreview
+                py={2}
+                px={4}
+                _hover={{
+                  background: useColorModeValue("gray.100", "gray.700"),
+                }}
+              />
+            </Tooltip>
             <Input py={2} px={4} as={EditableInput} />
             <EditableControls />
           </Editable>
@@ -83,7 +84,7 @@ const DetailsPanel = () => {
 
         <Box className="current-streak">
           <Wrap spacing="5px">
-            <WrapItem alignItems="center" >
+            <WrapItem alignItems="center">
               <FaFire color="#f05d4d" />
               <Text ml="5px">You are currently on a ____ streak</Text>
             </WrapItem>
@@ -91,15 +92,18 @@ const DetailsPanel = () => {
         </Box>
         <Box className="longest-streak">
           <Wrap spacing="5px">
-            <WrapItem alignItems="center" >
-              <FaTrophy color="gold"/>
+            <WrapItem alignItems="center">
+              <FaTrophy color="gold" />
               <Text ml="7px">Your longest streak is ____ days</Text>
             </WrapItem>
           </Wrap>
         </Box>
 
         <Box className="description">
-          <Heading size="sm" display="inline"> Description </Heading>
+          <Heading size="sm" display="inline">
+            {" "}
+            Description{" "}
+          </Heading>
           <Editable
             defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua"
             textAlign="center"
@@ -107,17 +111,16 @@ const DetailsPanel = () => {
             // selectAllOnFocus={false}
           >
             <Tooltip label="Click to edit">
-          <EditablePreview
-            py={2}
-            px={4}
-            _hover={{
-              background: useColorModeValue("gray.100", "gray.700")
-            }}
-          />
-        </Tooltip>
+              <EditablePreview
+                py={2}
+                px={4}
+                _hover={{
+                  background: useColorModeValue("gray.100", "gray.700"),
+                }}
+              />
+            </Tooltip>
             <EditableControls />
-            <EditableTextarea
-            rows="4" />
+            <EditableTextarea rows="4" />
           </Editable>
         </Box>
 
@@ -129,7 +132,7 @@ const DetailsPanel = () => {
         </Box>
       </VStack>
 
-      <Box className="frequency" >
+      <Box className="frequency">
         <Stack spacing={1} direction="row" align="baseline">
           <Text fontWeight="bold"> Frequency</Text>
           <Editable
@@ -139,30 +142,61 @@ const DetailsPanel = () => {
             borderWidth="3px"
             defaultValue="1"
             borderColor="orange"
-            
           >
             <EditablePreview />
             <EditableInput />
           </Editable>
 
           <Text fontWeight="bold">Times</Text>
-          <Select variant="outline" size="md" borderRadius="0.5em"
-            borderWidth="3px" borderColor="orange">
+          <Select
+            variant="outline"
+            size="md"
+            borderRadius="0.5em"
+            borderWidth="3px"
+            borderColor="orange"
+          >
             <option>Weekly</option>
             <option>Monthly</option>
           </Select>
         </Stack>
       </Box>
-
-      {/* Habit Title header , use theme font */}
-      {/* Current Habit Streak / Icon and p tag */}
-      {/* Longest Habit Streak / Icon and p tag*/}
-
-      {/* Description / p tag */}
-      {/* Everyday Toggle  / checkbox */}
-      {/* Frequency / p tag with input field and dropdown */}
+      <Box>
+        <VStack>
+          <Button
+            className="save-button"
+            bgRepeat="repeat"
+            colorScheme="orange"
+            bgGradient="linear(to-l, #f05d4d, #f8a642 )"
+            align="center"
+            direction="row"
+            mt="10px"
+          >
+            Save
+          </Button>
+        </VStack>
+      </Box>
     </Box>
   );
 };
 
 export default DetailsPanel;
+
+{
+  /* Habit Title header , use theme font */
+}
+{
+  /* Current Habit Streak / Icon and p tag */
+}
+{
+  /* Longest Habit Streak / Icon and p tag*/
+}
+
+{
+  /* Description / p tag */
+}
+{
+  /* Everyday Toggle  / checkbox */
+}
+{
+  /* Frequency / p tag with input field and dropdown */
+}
