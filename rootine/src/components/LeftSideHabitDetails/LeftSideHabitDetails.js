78e3f7ecf88f352012efc8d5ffb56cdb3ff9c49f
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-08-05 14:38:03 +0100

    move habit details panel styling from App.js into new component called LeftSideHabitDetails

---
+++ rootine/src/components/LeftSideHabitDetails/LeftSideHabitDetails.js
@@ -0,0 +1,40 @@
+import UploadHabit from "../UploadHabit/UploadHabit";
+import DetailsPanel from "../DetailsPanel/DetailsPanel";
+//prettier-ignore
+import { Box, Flex,	Container, SimpleGrid, GridItem, Wrap } from "@chakra-ui/react";
+
+export default function LeftSideHabitDetails() {
+	return (
+		<Box>
+			<SimpleGrid columns={2}>
+				<Container centerContent>
+					<GridItem>
+						<Flex
+							className="view-container"
+							height="100%"
+							width="auto"
+							display="flex"
+							alignItems="center"
+							marginLeft="5em"
+							marginBottom="1em">
+							<DetailsPanel />
+						</Flex>
+					</GridItem>
+				</Container>
+				<GridItem colStart={4} colEnd={4}>
+					<Flex
+						className="AddHabit-container"
+						//  height="55%"
+						//  width="55%"
+						// display="flex"
+						//  alignItems="center"
+						//  marginLeft="4em"
+						//  marginBottom="1em"
+					>
+						<UploadHabit />
+					</Flex>
+				</GridItem>
+			</SimpleGrid>
+		</Box>
+	);
+}
