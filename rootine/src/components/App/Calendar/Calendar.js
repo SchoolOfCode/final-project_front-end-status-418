3b11d96ed82cb09558c5af31b887611759b0978e
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-08-08 12:01:06 +0100

    stuff

--- rootine/src/components/App/Calendar/Calendar.js
+++ rootine/src/components/App/Calendar/Calendar.js
@@ -2,46 +2,71 @@ import { Container, Box, Heading, Button } from "@chakra-ui/react";
 import { CalendarBar } from "../../CalendarBar/CalendarBar";
 import "./Calendar.css";
 import HabitRow from "./HabitRow/HabitRow";
+import { useEffect, useState } from "react";
+import { default as dayjs } from "dayjs";
 
 const Calendar = () => {
-  let name = "Robert";
-  return (
-    <Container
-      className="calendar-view"
-      color="black"
-      overflow="hidden"
-      p="10"
-      maxW="75vw"
-    >
-      <Box as="div">
-        <Heading as="h3" size="lg">
-          Welcome, {name}
-        </Heading>
+    let name = "Robert";
 
-        <Box
-          className="calendar-bar-container"
-          mb="20px"
-          justifyContent="flex-end"
-          border="2px"
-          borderColor="red"
-          pl="130px"
+    const [daysOfWeek, setDaysOfWeek] = useState([]);
+    const [section, setSection] = useState(daysOfWeek.slice(0, 3));
+
+    const getCurrentWeekDays = () => {
+        const weekStart = dayjs().startOf("week");
+        const days = [];
+        for (let i = -5; i <= 100; i++) {
+            days.push(dayjs(weekStart).add(i, "days"));
+        }
+        return days;
+    };
+
+    useEffect(() => {
+        setDaysOfWeek(getCurrentWeekDays());
+    }, []);
+
+    console.log("days: ",getCurrentWeekDays());
+    console.log("DoW: ", daysOfWeek);
+    return (
+        <Container
+            className="calendar-view"
+            color="black"
+            overflow="hidden"
+            p="10"
+            maxW="75vw"
         >
-          <CalendarBar />
-        </Box>
-        <Box>
-          <HabitRow />
-          <HabitRow />
-          <HabitRow />
-          <HabitRow />
-          <HabitRow />
-        </Box>
+            <Box as="div">
+                <Heading as="h3" size="lg">
+                    Welcome, {name}
+                </Heading>
 
-        <Button bgGradient={["linear(to-l, red.400, orange.300)"]}>
-          Add +
-        </Button>
-      </Box>
-    </Container>
-  );
+                <Box
+                    className="calendar-bar-container"
+                    mb="20px"
+                    justifyContent="flex-end"
+                    border="2px"
+                    borderColor="red"
+                    pl="130px"
+                >
+                    <CalendarBar
+                        dayList={daysOfWeek}
+                        section={section}
+                        setSection={setSection}
+                    />
+                </Box>
+                <Box>
+                    <HabitRow />
+                    <HabitRow />
+                    <HabitRow />
+                    <HabitRow />
+                    <HabitRow />
+                </Box>
+
+                <Button bgGradient={["linear(to-l, red.400, orange.300)"]}>
+                    Add +
+                </Button>
+            </Box>
+        </Container>
+    );
 };
 
 export default Calendar;
--- rootine/src/components/CalendarBar/CalendarBar.js
+++ rootine/src/components/CalendarBar/CalendarBar.js
@@ -1,5 +1,4 @@
 import { default as dayjs } from "dayjs";
-import { useEffect, useState } from "react";
 import "../CalendarBar/CalendarBar.css";
 import React from "react";
 
@@ -8,154 +7,158 @@ Edit Menu Item component
 Replace buttons with new functionality 
 Remove onselect function and Scroll menu component 
 render menu item with new buttons 
-
-
 */
 
-export const CalendarBar = () => {
-  const [daysOfweek, setDaysOfWeek] = useState([]);
-  const [section, setSection] = useState(daysOfweek.slice(0, 3));
-  const currentDay = dayjs().format("DDMM");
+export const CalendarBar = ({dayList, section, setSection}) => {
 
-  const getCurrentWeekDays = () => {
-    const weekStart = dayjs().startOf("week");
+/*     const [daysOfweek, setDaysOfWeek] = useState([]);
+    const [section, setSection] = useState(daysOfweek.slice(0, 3));
+    const currentDay = dayjs().format("DDMM");
 
-    const days = [];
-    for (let i = -5; i <= 100; i++) {
-      days.push(dayjs(weekStart).add(i, "days"));
+    const getCurrentWeekDays = () => {
+        const weekStart = dayjs().startOf("week");
+
+        const days = [];
+        for (let i = -5; i <= 100; i++) {
+            days.push(dayjs(weekStart).add(i, "days"));
+        }
+
+        return days;
+    };
+
+    useEffect(() => {
+        setDaysOfWeek(getCurrentWeekDays());
+    }, []); */
+    const currentDay = dayjs().format("DDMM");
+    console.log("daylist (cb): ",dayList)
+    const CalendarItem = ({ title, text, selected, now }) => {
+        return (
+            <div
+                className={`menu-item dayItem ${selected ? "active" : ""} ${
+                    currentDay === now ? "today" : null
+                }`}
+            >
+                <h5 className="title">{title}</h5>
+                <span className="text"> {text}</span>
+            </div>
+        );
+    };
+
+    function LeftArrow() {
+        const getPrevSection = () => {
+            let firstItem = section[0];
+            const dataCopy = [...dayList];
+            let indexOfFirstItem = dataCopy.findIndex(
+                (element) => element === firstItem
+            );
+
+            console.log("dataCopy", dataCopy);
+            console.log("indexOfFirstItem", indexOfFirstItem);
+
+            // if there are 3 more items to display
+            if (indexOfFirstItem - 3 >= 0) {
+                let prevSnapshot = dataCopy.slice(
+                    indexOfFirstItem - 3,
+                    indexOfFirstItem
+                );
+                console.log("nextSnapshot", prevSnapshot);
+                setSection(prevSnapshot);
+                // if there are two more items to display
+            } else if (indexOfFirstItem - 2 >= 0) {
+                let prevSnapshot = dataCopy.slice(
+                    indexOfFirstItem - 2,
+                    indexOfFirstItem
+                );
+                console.log("nextSnapshot", prevSnapshot);
+                setSection(prevSnapshot);
+                // if there is one more item to display
+            } else if (indexOfFirstItem - 1 >= 0) {
+                let prevSnapshot = dataCopy.slice(
+                    indexOfFirstItem - 1,
+                    indexOfFirstItem
+                );
+                console.log("nextSnapshot", prevSnapshot);
+                setSection(prevSnapshot);
+            }
+        };
+        return (
+            <button className="left-button" onClick={getPrevSection}>
+                Prev
+            </button>
+        );
     }
 
-    return days;
-  };
+    function RightArrow() {
+        const getNextSection = () => {
+            const dataCopy = [...dayList];
+            let lastItem = section[section.length - 1];
+            let indexOfLastItem = dataCopy.findIndex(
+                (element) => element === lastItem
+            );
+            let firstItemOfSnapshot = indexOfLastItem + 1;
 
-  useEffect(() => {
-    setDaysOfWeek(getCurrentWeekDays());
-  }, []);
+            console.log("dataCopy", dataCopy);
+            console.log("indexOfLastItemy", indexOfLastItem);
 
-  const CalendarItem = ({ title, text, selected, now }) => {
+            // if there are 3 more items to display
+            if (firstItemOfSnapshot + 3 <= dataCopy.length) {
+                let nextSnapshot = dataCopy.slice(
+                    firstItemOfSnapshot,
+                    firstItemOfSnapshot + 3
+                );
+                console.log("nextSnapshot", nextSnapshot);
+
+                setSection(nextSnapshot);
+                // if there are two more items to display
+            } else if (firstItemOfSnapshot + 2 <= dataCopy.length) {
+                let nextSnapshot = dataCopy.slice(
+                    firstItemOfSnapshot,
+                    firstItemOfSnapshot + 2
+                );
+                console.log("nextSnapshot", nextSnapshot);
+
+                setSection(nextSnapshot);
+                // if there is one more item to display
+            } else if (firstItemOfSnapshot + 1 <= dataCopy.length) {
+                let nextSnapshot = dataCopy.slice(
+                    firstItemOfSnapshot,
+                    firstItemOfSnapshot + 1
+                );
+                console.log("nextSnapshot", nextSnapshot);
+
+                setSection(nextSnapshot);
+            }
+        };
+        return (
+            <button className="right-button" onClick={getNextSection}>
+                Next
+            </button>
+        );
+    }
+    console.log(dayList)
     return (
-      <div
-        className={`menu-item dayItem ${selected ? "active" : ""} ${
-          currentDay === now ? "today" : null
-        }`}
-      >
-        <h5 className="title">{title}</h5>
-        <span className="text"> {text}</span>
-      </div>
+        <div className=" calendar-bar-main">
+            <LeftArrow />
+            <div className="calendar-items-container">
+                {section.map((day) => {
+                    return (
+                        <CalendarItem
+                            itemId={day.format("D")}
+                            title={day.format("MMM")}
+                            text={day.format("DD")}
+                            key={day.format("YYYYMMDD")}
+                            id={day.format("YYYYMMDD")}
+                            now={day.format("DDMM")}
+                        />
+                    );
+                })}
+            </div>
+
+            <RightArrow />
+        </div>
     );
-  };
-
-  function LeftArrow() {
-    const getPrevSection = () => {
-      let firstItem = section[0];
-      const dataCopy = [...daysOfweek];
-      let indexOfFirstItem = dataCopy.findIndex(
-        (element) => element === firstItem
-      );
-
-      console.log("dataCopy", dataCopy);
-      console.log("indexOfFirstItem", indexOfFirstItem);
-
-      // if there are 3 more items to display
-      if (indexOfFirstItem - 3 >= 0) {
-        let prevSnapshot = dataCopy.slice(
-          indexOfFirstItem - 3,
-          indexOfFirstItem
-        );
-        console.log("nextSnapshot", prevSnapshot);
-        setSection(prevSnapshot);
-        // if there are two more items to display
-      } else if (indexOfFirstItem - 2 >= 0) {
-        let prevSnapshot = dataCopy.slice(
-          indexOfFirstItem - 2,
-          indexOfFirstItem
-        );
-        console.log("nextSnapshot", prevSnapshot);
-        setSection(prevSnapshot);
-        // if there is one more item to display
-      } else if (indexOfFirstItem - 1 >= 0) {
-        let prevSnapshot = dataCopy.slice(
-          indexOfFirstItem - 1,
-          indexOfFirstItem
-        );
-        console.log("nextSnapshot", prevSnapshot);
-        setSection(prevSnapshot);
-      }
-    };
-    return (
-      <button className="left-button" onClick={getPrevSection}>
-        Prev
-      </button>
-    );
-  }
-
-  function RightArrow() {
-    const getNextSection = () => {
-      const dataCopy = [...daysOfweek];
-      let lastItem = section[section.length - 1];
-      let indexOfLastItem = dataCopy.findIndex(
-        (element) => element === lastItem
-      );
-      let firstItemOfSnapshot = indexOfLastItem + 1;
-
-      console.log("dataCopy", dataCopy);
-      console.log("indexOfLastItemy", indexOfLastItem);
-
-      // if there are 3 more items to display
-      if (firstItemOfSnapshot + 3 <= dataCopy.length) {
-        let nextSnapshot = dataCopy.slice(
-          firstItemOfSnapshot,
-          firstItemOfSnapshot + 3
-        );
-        console.log("nextSnapshot", nextSnapshot);
-
-        setSection(nextSnapshot);
-        // if there are two more items to display
-      } else if (firstItemOfSnapshot + 2 <= dataCopy.length) {
-        let nextSnapshot = dataCopy.slice(
-          firstItemOfSnapshot,
-          firstItemOfSnapshot + 2
-        );
-        console.log("nextSnapshot", nextSnapshot);
-
-        setSection(nextSnapshot);
-        // if there is one more item to display
-      } else if (firstItemOfSnapshot + 1 <= dataCopy.length) {
-        let nextSnapshot = dataCopy.slice(
-          firstItemOfSnapshot,
-          firstItemOfSnapshot + 1
-        );
-        console.log("nextSnapshot", nextSnapshot);
-
-        setSection(nextSnapshot);
-      }
-    };
-    return (
-      <button className="right-button" onClick={getNextSection}>
-        Next
-      </button>
-    );
-  }
-
-  return (
-    <div className=" calendar-bar-main">
-      <LeftArrow />
-      <div className="calendar-items-container">
-        {section.map((day) => {
-          return (
-            <CalendarItem
-              itemId={day.format("D")}
-              title={day.format("MMM")}
-              text={day.format("DD")}
-              key={day.format("YYYYMMDD")}
-              id={day.format("YYYYMMDD")}
-              now={day.format("DDMM")}
-            />
-          );
-        })}
-      </div>
-
-      <RightArrow />
-    </div>
-  );
 };
+
+/* 
+
+*/
