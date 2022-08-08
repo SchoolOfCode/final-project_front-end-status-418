7e9a0b61d5484182ad619a41284b8e135ea7c61e
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-08-08 16:23:05 +0100

    continue WIP for creating new hbait item for calendar

--- rootine/src/components/App/Calendar/Calendar.js
+++ rootine/src/components/App/Calendar/Calendar.js
@@ -6,108 +6,106 @@ import { useEffect, useState } from "react";
 import { default as dayjs } from "dayjs";
 
 const habits = [
-  {
-    id: 2,
-    name: "Drink more water",
-    description: "Keep a jug full of water by my desk",
-    userId: "xyz123",
-    everyday: true,
-    frequency: {
-      fr_reps: null,
-      fr_interval: null,
-    },
-  },
-  {
-    id: 3,
-    name: "Dry August",
-    description: "No alcholic drinks",
-    userId: "sam123",
-    everyday: true,
-    frequency: {
-      fr_reps: null,
-      fr_interval: null,
-    },
-  },
-  {
-    id: 1,
-    name: "Walk the dog",
-    description: "Every morning and evening, I'll grab the dog's leash",
-    userId: "abc123",
-    everyday: true,
-    frequency: {
-      fr_reps: null,
-      fr_interval: null,
-    },
-  },
+	{
+		id: 2,
+		name: "Drink more water",
+		description: "Keep a jug full of water by my desk",
+		userId: "xyz123",
+		everyday: true,
+		frequency: {
+			fr_reps: null,
+			fr_interval: null,
+		},
+	},
+	{
+		id: 3,
+		name: "Dry August",
+		description: "No alcholic drinks",
+		userId: "sam123",
+		everyday: true,
+		frequency: {
+			fr_reps: null,
+			fr_interval: null,
+		},
+	},
+	{
+		id: 1,
+		name: "Walk the dog",
+		description: "Every morning and evening, I'll grab the dog's leash",
+		userId: "abc123",
+		everyday: true,
+		frequency: {
+			fr_reps: null,
+			fr_interval: null,
+		},
+	},
 ];
 
 const Calendar = () => {
-  let name = "Robert";
+	let name = "Robert";
 
-  const [daysOfWeek, setDaysOfWeek] = useState([]);
-  const [section, setSection] = useState(daysOfWeek.slice(0, 3));
+	const [daysOfWeek, setDaysOfWeek] = useState([]);
+	const [section, setSection] = useState(daysOfWeek.slice(0, 3));
 
-  const getCurrentWeekDays = () => {
-    const weekStart = dayjs().startOf("week");
-    const days = [];
-    for (let i = -5; i <= 100; i++) {
-      days.push(dayjs(weekStart).add(i, "days"));
-    }
-    return days;
-  };
+	const getCurrentWeekDays = () => {
+		const weekStart = dayjs().startOf("week");
+		const days = [];
+		for (let i = -5; i <= 100; i++) {
+			days.push(dayjs(weekStart).add(i, "days"));
+		}
+		return days;
+	};
 
-  useEffect(() => {
-    setDaysOfWeek(getCurrentWeekDays());
-  }, []);
+	useEffect(() => {
+		setDaysOfWeek(getCurrentWeekDays());
+	}, []);
 
-  console.log("days: ", getCurrentWeekDays());
-  console.log("DoW: ", daysOfWeek);
-  return (
-    <Container
-      className="calendar-view"
-      color="black"
-      overflow="hidden"
-      p="10"
-      maxW="75vw"
-    >
-      <Box as="div">
-        <Heading as="h3" size="lg">
-          Welcome, {name}
-        </Heading>
+	// console.log("days: ", getCurrentWeekDays());
+	//   console.log("DoW: ", daysOfWeek);
+	return (
+		<Container
+			className="calendar-view"
+			color="black"
+			overflow="hidden"
+			p="10"
+			maxW="75vw">
+			<Box as="div">
+				<Heading as="h3" size="lg">
+					Welcome, {name}
+				</Heading>
 
-        <Box
-          className="calendar-bar-container"
-          mb="20px"
-          justifyContent="flex-end"
-          border="2px"
-          borderColor="red"
-          pl="130px"
-        >
-          <CalendarBar
-            dayList={daysOfWeek}
-            section={section}
-            setSection={setSection}
-          />
-        </Box>
-        <Box>
-          {habits.map((habit) => {
-            return (
-              <HabitRow
-                habitName={habit.name}
-                key={habit.id}
-                habitid={habit.id}
-                section={section}
-              />
-            );
-          })}
-        </Box>
+				<Box
+					className="calendar-bar-container"
+					mb="20px"
+					justifyContent="flex-end"
+					border="2px"
+					borderColor="red"
+					pl="130px">
+					<CalendarBar
+						dayList={daysOfWeek}
+						section={section}
+						setSection={setSection}
+					/>
+				</Box>
+				<Box>
+					{habits.map((habit) => {
+						return (
+							<HabitRow
+								habitName={habit.name}
+								key={habit.id}
+								habitid={habit.id}
+								section={section}
+							/>
+						);
+					})}
+				</Box>
 
-        <Button bgGradient={["linear(to-l, red.400, orange.300)"]}>
-          Add +
-        </Button>
-      </Box>
-    </Container>
-  );
+				<Button bgGradient={["linear(to-l, red.400, orange.300)"]}>
+					Add +
+				</Button>
+			</Box>
+		</Container>
+	);
 };
 
 export default Calendar;
--- rootine/src/components/App/Calendar/HabitRow/HabitRow.js
+++ rootine/src/components/App/Calendar/HabitRow/HabitRow.js
@@ -2,174 +2,200 @@ import { useState } from "react";
 import "./HabitRow.css";
 
 function HabitRow({ habitName, habitid, section }) {
-  /*Mock Data */
-  const habitArr = [
-    {
-      habit_id: 1,
-      date: "20220807",
-      status: "complete",
-    },
-    {
-      habit_id: 1,
-      date: "20220806",
-      status: "complete",
-    },
-    {
-      habit_id: 1,
-      date: "20220805",
-      status: "complete",
-    },
-    {
-      habit_id: 1,
-      date: "20220804",
-      status: "complete",
-    },
-    {
-      habit_id: 1,
-      date: "20220803",
-      status: "complete",
-    },
-    {
-      habit_id: 1,
-      date: "20220802",
-      status: "complete",
-    },
-    {
-      habit_id: 1,
-      date: "20220801",
-      status: "skip",
-    },
-    {
-      habit_id: 1,
-      date: "20220731",
-      status: "complete",
-    },
-    {
-      habit_id: 1,
-      date: "20220730",
-      status: "miss",
-    },
-  ];
+	/*Mock Data */
+	const habitArr = [
+		{
+			habit_id: 1,
+			date: "20220807",
+			status: "complete",
+		},
+		{
+			habit_id: 1,
+			date: "20220806",
+			status: "complete",
+		},
+		{
+			habit_id: 1,
+			date: "20220805",
+			status: "complete",
+		},
+		{
+			habit_id: 1,
+			date: "20220804",
+			status: "complete",
+		},
+		{
+			habit_id: 1,
+			date: "20220803",
+			status: "complete",
+		},
+		{
+			habit_id: 1,
+			date: "20220802",
+			status: "complete",
+		},
+		{
+			habit_id: 1,
+			date: "20220801",
+			status: "skip",
+		},
+		{
+			habit_id: 1,
+			date: "20220731",
+			status: "complete",
+		},
+		{
+			habit_id: 1,
+			date: "20220730",
+			status: "miss",
+		},
+	];
 
-  const [habitItems, setHabitItems] = useState(habitArr);
+	const [habitItems, setHabitItems] = useState(habitArr);
 
-  /* Function which takes in habit state (habitObj), new value of status, id of habitItemList obj to change,
+	/** Function that takes in habit state (habitObj), new value of status, id of habitItemList obj to change,
      and returns state with updated status  
   */
-  const changeStatus = (state, newStatus, id) => {
-    // shallow copy of habitObj
-    let newArr = [...state];
+	const changeStatus = (state, newStatus, id) => {
+		// shallow copy of habitObj
+		let newArr = [...state];
 
-    // index of habitItem we want to change
-    let indexOfObject = newArr.findIndex((element) => element.date === id);
-    // changed habitItem
-    let updatedObj = { ...newArr[indexOfObject], status: newStatus };
+		// index of habitItem we want to change
+		let indexOfObject = newArr.findIndex((element) => element.date === id);
+		// changed habitItem
+		let updatedObj = { ...newArr[indexOfObject], status: newStatus };
 
-    // making a duplicate of whole habitItems array before & after position of our Item
-    let firstChunk = newArr.slice(0, indexOfObject);
-    let secondChunk = newArr.slice(indexOfObject + 1);
+		// making a duplicate of whole habitItems array before & after position of our Item
+		let firstChunk = newArr.slice(0, indexOfObject);
+		let secondChunk = newArr.slice(indexOfObject + 1);
 
-    // inserting our updated item into duplicate array
-    let updatedArr = [...firstChunk, updatedObj, ...secondChunk];
+		// inserting our updated item into duplicate array
+		let updatedArr = [...firstChunk, updatedObj, ...secondChunk];
 
-    //console.log(" updatedArr", updatedArr);
-    return updatedArr;
-  };
+		return updatedArr;
+	};
 
-  /* Function which takes in id and when called checks the value of status for particular 
+	/** Function that takes in id and when called checks the value of status for particular 
 habitItem and changes it accordingly
 */
-  function toggleState(id) {
-    /* defining the index of the specific object in habitItemList that we are toggling */
-    //console.log("toggleState called here.")
-    console.log("id", id);
-    let habitCopy = [...habitItems];
-    console.log("habitCopy", habitCopy);
-    let index = habitCopy.findIndex((element) => element.date === id);
-    console.log("index", index);
-    /* defining the status property we want to change using the above index */
-    let status = habitCopy[index].status;
-    //console.log("(Before) Status: "+ habit.habitItemList[index].status)
+	function toggleState(currentDate) {
+		/* defining the index of the specific object in habitItemList that we are toggling */
+		console.log("currentDate", currentDate);
+		let habitCopy = [...habitItems];
+		// console.log("habitCopy", habitCopy);
+		let index = habitCopy.findIndex(
+			(element) => element.date === currentDate
+		);
+		console.log("index", index);
 
-    let updatedState = [];
+		// 📝 PLAN
+		// if index is not found (aka returns -1, aka date does not exist in habitArray)
+		// then, send a post request to the db with the new creation
+		// then, do not do rest of toggleState function
 
-    switch (status) {
-      case "incomplete":
-        updatedState = changeStatus(habitItems, "complete", id);
-        break;
-      case "complete":
-        updatedState = changeStatus(habitItems, "skip", id);
-        break;
-      case "skip":
-        updatedState = changeStatus(habitItems, "miss", id);
-        break;
-      default:
-        updatedState = changeStatus(habitItems, "incomplete", id);
-        break;
-    }
-    console.log("(Before) Status: ", habitCopy[index].status);
-    console.log(" (After) Status: ", updatedState[index].status);
-    setHabitItems(updatedState);
-  }
-  try {
-    return (
-      <div className="habit-row">
-        <div className="habit-name-container">
-          <h3 className="habit-name"> {habitName} </h3>
-        </div>
-        <div className="habit-item-container">
-          {section.map((sectionday) => {
-            console.log("habitItem", habitItems[0].date);
-            console.log("sectiondayformatted", sectionday.format("YYYYMMDD"));
-            let displayItem = habitItems.filter(
-              (item) => item.date === sectionday.format("YYYYMMDD")
-            );
+		if (index === -1) {
+			//define new data
+			const newHabitCalItem = {
+				habit_id: habitid,
+				date: currentDate,
+				status: "complete",
+			};
+			
+      //TODO: SEND THIS TO NEW ASYNC Function
+      // ❌ NOt working yet :)
+      //send post request with data in correct format
+			const url = "https://status418-project.herokuapp.com/";
 
-            console.log("displayItem", displayItem);
-            return (
-              <button
-                onClick={() => toggleState(displayItem[0].date)}
-                className={`habit-item ${displayItem[0].status}`}
-                id={sectionday.format("YYYYMMDD")}
-                key={sectionday.format("YYYYMMDD")}
-              />
-            );
-          })}
-        </div>
-      </div>
-    );
-  } catch (error) {
-    console.log(error);
-  }
+			const result = await fetch(url + "/calendar", {
+				method: "POST",
+				headers: { "Content-type": "application/json" },
+				body: newHabitCalItem,
+			});
+
+			console.log(result);
+		}
+
+		/* defining the status property we want to change using the above index */
+		let status = habitCopy[index].status;
+		let updatedState = [];
+
+		switch (status) {
+			case "incomplete":
+				updatedState = changeStatus(
+					habitItems,
+					"complete",
+					currentDate
+				);
+				break;
+			case "complete":
+				updatedState = changeStatus(habitItems, "skip", currentDate);
+				break;
+			case "skip":
+				updatedState = changeStatus(habitItems, "miss", currentDate);
+				break;
+			default:
+				updatedState = changeStatus(
+					habitItems,
+					"incomplete",
+					currentDate
+				);
+				break;
+		}
+		// console.log("(Before) Status: ", habitCopy[index].status);
+		// console.log(" (After) Status: ", updatedState[index].status);
+		setHabitItems(updatedState);
+	}
+
+	// 📝 PLAN
+	// When habit item doesn't exist, needs to
+	// (1) Still display a white box on the page
+	// (2) When clicked, create a new entry in the calendar database with status: "complete"
+	// (3) Then, it can be interacted with normally because the data will exist.
+
+	// 🙋‍♂️ FIRST: Even if the data doesn't exist in the habitArry, a white square (aka button with className = "incomplete") should still exist
+	// 🤞 SECOND: Send a post request when its clicked on
+
+	try {
+		return (
+			<div className="habit-row">
+				<div className="habit-name-container">
+					<h3 className="habit-name"> {habitName} </h3>
+				</div>
+				<div className="habit-item-container">
+					{section.map((sectionday) => {
+						const ymd = sectionday.format("YYYYMMDD");
+						console.log("habitItem", habitItems[0].date);
+						console.log("sectiondayformatted", ymd);
+						let displayItem = habitItems.filter(
+							(item) => item.date === ymd
+						);
+
+						if (displayItem.length < 1) {
+							displayItem = [
+								{
+									status: "incomplete",
+									date: ymd,
+									habit_id: habitid,
+								},
+							];
+						}
+
+						console.log("displayItem", displayItem);
+						return (
+							<button
+								onClick={() => toggleState(displayItem[0].date)}
+								className={`habit-item ${displayItem[0].status}`}
+								id={ymd}
+								key={ymd}
+							/>
+						);
+					})}
+				</div>
+			</div>
+		);
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 export default HabitRow;
-
-/*
-function HabitItem () => {
-    const [status,setStatus] = useState("Incomplete")
-    function toggleState () {
-        switch (status) {
-            case "Incomplete":
-                setStatus("Complete")
-                break;
-            case "Complete":
-                setStatus("Skip")
-                break;
-            case "Skip":
-                setStatus("Miss")
-                break;
-            default:
-                setStatus("Incomplete")
-                break;
-        }
-    }
-    return (
-        <button
-            onClick={toggleState}
-            className="habit-item"
-            id={status}
-        />
-        )
-    }
-*/
