89a1ec26ea43efdce90a205ec74e97e6e4004ff4
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-08-11 16:51:52 +0100

    Fix for new user flow in progress

--- rootine/src/components/App/App.js
+++ rootine/src/components/App/App.js
@@ -13,6 +13,41 @@ import Calendar from "./Calendar/Calendar";
 import { Flex } from "@chakra-ui/react"
 import { flexProps } from "./appProps.js";
 
+/* 
+    Upon logging in, check whether incoming user_id matches already exists
+        If it does, do nothing
+        If it doesn't, send post req 
+*/
+
+async function fetchAllUsers () {
+    let response = await fetch ("https://status418-project.herokuapp.com/user")
+    let data = await response.json()
+    return data.payload
+}
+
+async function checkIncomingUserid (user) {
+    let userlist =  await fetchAllUsers()
+    for (let i=0;i<userlist.length;i++) {
+        if (userlist[i].user_id===user.sub.substr(6)) {
+            return
+        } else {
+            await fetch ("https://status418-project.herokuapp.com/user", {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json",
+                    "Access-Control-Allow-Origin": "*",
+                },
+                body: JSON.stringify({
+                    username: `${user.nickname}`,
+                    user_id: `${user.sub.substr(6)}`,
+                }),
+            })
+        }
+
+    }
+}
+
+
 function App() {
 	const [currentHabitDisplayed, setCurrentHabitDisplayed] = useState([]);
 	const [isFormDisplayed, setIsFormDisplayed] = useState(false);
@@ -24,6 +59,7 @@ function App() {
 	}
 
 	const { user, isAuthenticated, isLoading } = useAuth0();
+    checkIncomingUserid (user)
 
 	if (isLoading) {
 		return (
@@ -45,6 +81,7 @@ function App() {
 			<main>
 				<p>Authenticated? {isAuthenticated ? "yes" : "no"}</p>
 				<p>{user ? "user = " + user.nickname : "no username info"}</p>
+                <p>{user ? "user = " + user.sub.substr(6) : "no id info"}</p>
 				{!isAuthenticated ? (
 					<LandingPage />
 				) : (
