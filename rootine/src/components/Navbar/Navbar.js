c5f60e7ed0b5497feeaae786e85ce11638f65a4d
Samantha Wu <samanthayywu@gmail.com>
2022-08-15 17:16:29 +0100

    vars hidden

--- rootine/src/components/Navbar/Navbar.js
+++ rootine/src/components/Navbar/Navbar.js
@@ -1,19 +1,19 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import "./Navbar.css";
-import { useColorMode } from "@chakra-ui/react";
-import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+// import { useColorMode } from "@chakra-ui/react";
+// import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 import { useAuth0 } from "@auth0/auth0-react";
 
 //prettier-ignore
-import { Menu, MenuButton, MenuList, MenuItem, MenuGroup, MenuDivider, Button } from "@chakra-ui/react";
+import { Menu, MenuButton, MenuList, MenuItem, MenuDivider, Button } from "@chakra-ui/react";
 
 export default function Navbar() {
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
   // console.log("isAuthenticated", isAuthenticated);
   // console.log("user", user);
 
-  const { colorMode, toggleColorMode } = useColorMode();
+  //   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <header className="header">
       <div className="header-wrapper">
