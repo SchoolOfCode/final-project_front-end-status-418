ce8eb227e75e2450ac6d972cc1b83e0563d3b145
Samantha Wu <samanthayywu@gmail.com>
2022-08-15 15:49:33 +0100

    working on chakra modal

--- rootine/src/components/Footer/Footer.js
+++ rootine/src/components/Footer/Footer.js
@@ -1,23 +1,53 @@
 import "./Footer.css";
+
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+} from "@chakra-ui/react";
+
 const Footer = () => {
-	return (
-		<footer className="footer">
-			{/* 3 links- privacy policy, blog, about */}
-			<div className="footer-border"></div>
-			<ul>
-				<li>
-					<a href="Privacy">Privacy Policy</a>
-				</li>
-				<li>
-					<a href="Blog">Blog</a>
-				</li>
-				<li>
-					<a href="About">About</a>
-				</li>
-			</ul>
-			<p className="copyright">&copy; 2022 Status 418</p>
-		</footer>
-	);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <footer className="footer">
+      {/* 3 links- privacy policy, blog, about */}
+      <div className="footer-border"></div>
+      <ul>
+        <li>
+          const {(isOpen, onOpen, onClose)} = useDisclosure()
+          <Button onClick={onOpen}>Open Modal</Button>
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Modal Title</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>{/* <Lorem count={2} /> */}</ModalBody>
+
+              <ModalFooter>
+                <Button colorScheme="blue" mr={3} onClick={onClose}>
+                  Close
+                </Button>
+                <Button variant="ghost">Secondary Action</Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </li>
+        <li>
+          <a href="Blog">Blog</a>
+        </li>
+        <li>
+          <a href="About">About</a>
+        </li>
+      </ul>
+      <p className="copyright">&copy; 2022 Status 418</p>
+    </footer>
+  );
 };
 
 export default Footer;
