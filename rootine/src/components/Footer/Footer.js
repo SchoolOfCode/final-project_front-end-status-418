b61d511b2f073cfced5d9c379d77edf1a5ac7203
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-08-15 16:04:03 +0100

    initialise privacy policy modal

--- rootine/src/components/Footer/Footer.js
+++ rootine/src/components/Footer/Footer.js
@@ -1,53 +1,72 @@
 import "./Footer.css";
+import Policy from "../../Policy.js";
 
 import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  useDisclosure,
-  Button,
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalFooter,
+	ModalBody,
+	ModalCloseButton,
+	useDisclosure,
+	Button,
 } from "@chakra-ui/react";
 
-const Footer = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  return (
-    <footer className="footer">
-      {/* 3 links- privacy policy, blog, about */}
-      <div className="footer-border"></div>
-      <ul>
-        <li>
-          const {(isOpen, onOpen, onClose)} = useDisclosure()
-          <Button onClick={onOpen}>Open Modal</Button>
-          <Modal isOpen={isOpen} onClose={onClose}>
-            <ModalOverlay />
-            <ModalContent>
-              <ModalHeader>Modal Title</ModalHeader>
-              <ModalCloseButton />
-              <ModalBody>{/* <Lorem count={2} /> */}</ModalBody>
+const footerItemsProps = {
+	fontFamily: "Quando",
+	fontWeight: "normal",
+	fontSize: 17,
+	colorScheme: "#f8a642",
+	color: "#22553f",
+	_hover: { color: "#f05d4d", backgroundColor: "transparent" },
+	backgroundColor: "transparent",
+};
 
-              <ModalFooter>
-                <Button colorScheme="blue" mr={3} onClick={onClose}>
-                  Close
-                </Button>
-                <Button variant="ghost">Secondary Action</Button>
-              </ModalFooter>
-            </ModalContent>
-          </Modal>
-        </li>
-        <li>
-          <a href="Blog">Blog</a>
-        </li>
-        <li>
-          <a href="About">About</a>
-        </li>
-      </ul>
-      <p className="copyright">&copy; 2022 Status 418</p>
-    </footer>
-  );
+const Footer = () => {
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	return (
+		<footer className="footer">
+			{/* 3 links- privacy policy, blog, about */}
+			<div className="footer-border"></div>
+			<ul>
+				<li>
+					<Button onClick={onOpen} {...footerItemsProps}>
+						Privacy Policy
+					</Button>
+					<PrivacyModal isOpen={isOpen} onClose={onClose} />
+				</li>
+				<li>
+					<a href="Blog">Blog</a>
+				</li>
+				<li>
+					<a href="About">About</a>
+				</li>
+			</ul>
+			<p className="copyright">&copy; 2022 Status 418</p>
+		</footer>
+	);
 };
 
 export default Footer;
+
+function PrivacyModal({ isOpen, onClose }) {
+	return (
+		<Modal isOpen={isOpen} onClose={onClose} size="4xl">
+			<ModalOverlay />
+			<ModalContent>
+				<ModalHeader>Privacy Policy</ModalHeader>
+				<ModalCloseButton />
+				<ModalBody>
+					<Policy />
+				</ModalBody>
+
+				<ModalFooter>
+					<Button colorScheme="blue" mr={3} onClick={onClose}>
+						Close
+					</Button>
+				</ModalFooter>
+			</ModalContent>
+		</Modal>
+	);
+}
