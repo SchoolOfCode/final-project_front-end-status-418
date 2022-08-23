10d1d6f1d6ef1d9170399ae370027741cef79e4f
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-08-23 15:01:32 +0100

    added (basic) static tickertape with tooltip for overflow

--- rootine/src/components/Tickertape/Tickertape.js
+++ rootine/src/components/Tickertape/Tickertape.js
@@ -2,9 +2,14 @@ import React, { useState, useEffect } from "react";
 import Ticker from "react-ticker";
 import PageVisibility from "react-page-visibility";
 import { tickertapeAdvice } from "./libs";
-// import { Center } from "@chakra-ui/react";
+//prettier-ignore
+import { tickertapeProps, tickerContentProps, tooltipProps, } from "./tickertapeProps";
+import { Center, Tooltip } from "@chakra-ui/react";
 
-// function TickertapeContent() {
+// 📝 NOTE: Unable to get 'react-ticker' package working, causing infinite rerenders and idk why... Instead, it fetches a new piece of advice on pageload and displays it.
+
+//test
+// function TickertapeContent1() {
 // 	const [tickertapeContent, setTickertapeContent] = useState([]);
 
 // 	// function shuffleTickertapeContent() {
@@ -39,28 +44,84 @@ import { tickertapeAdvice } from "./libs";
 // 	);
 // }
 
-const Tickertape = () => {
+// function TickertapeContent2() {
+// 	// let randIndex = Math.floor(Math.random() * tickertapeAdvice.length);
+// 	// console.log("randIndex", randIndex);
+// 	// console.log(tickertapeAdvice[randIndex]);
+
+// 	const [advice, setAdvice] = useState("");
+
+// 	useEffect(() => {
+// 		function retrieveContent() {
+// 			setAdvice(tickertapeAdvice);
+// 		}
+// 		retrieveContent();
+// 	}, []);
+
+// 	return advice ? (
+// 		<p>{advice.join(" ... ")} ... </p>
+// 	) : (
+// 		<p style={{ visibility: "hidden" }}>Placeholder</p>
+// 	);
+// }
+
+// function TickertapeContent() {}
+
+export default function Tickertape() {
 	//Tickertape only runs when page is being looked at
 	// const [pageIsVisible, setPageIsVisible] = useState(true);
-
 	// function handleVisibilityChange(isVisible) {
 	// 	setPageIsVisible(isVisible);
 	// }
-
 	// const [tickertapeContent, setTickertapeContent] = useState([]);
+	// return (
+	// 	// <PageVisibility onChange={handleVisibilityChange}>
+	// 	// {pageIsVisible && (
+	// 	<div style={{ width: "100%", height: "2em" }}>
+	// 		<Ticker offset="run-in" mode="smooth">
+	// 			{(index) => (
+	// 				<h2>
+	// 					{
+	// 						tickertapeAdvice[
+	// 							Math.floor(
+	// 								Math.random() *
+	// 									(tickertapeAdvice.length - 1)
+	// 							)
+	// 						]
+	// 					}
+	// 				</h2>
+	// 			)}
+	// 		</Ticker>
+	// 		{/* <p>Tickertape</p> */}
+	// 	</div>
+	// 	// )}
+	// 	// </PageVisibility>
+	// );
+
+	const [advice, setAdvice] = useState("");
+
+	useEffect(() => {
+		function fetchRandAdvice() {
+			let randIndex = Math.floor(
+				Math.random() * (tickertapeAdvice.length - 1)
+			);
+			console.log("randIndex", randIndex);
+			console.log(tickertapeAdvice[randIndex]);
+			setAdvice(tickertapeAdvice[randIndex]);
+		}
+		fetchRandAdvice();
+	}, []);
 
 	return (
-		// <PageVisibility onChange={handleVisibilityChange}>
-		// {pageIsVisible && (
-		<>
-			{/* <Ticker offset="run-in" mode="smooth">
-				{() => <>"Tickertape content"</>}
-			</Ticker> */}
-			<p>Tickertape</p>
-		</>
-		// )}
-		// </PageVisibility>
+		<div style={{ width: "90vw", margin: "auto" }}>
+			<Center {...tickertapeProps}>
+				<Tooltip label={`HINT: ${advice}`} {...tooltipProps}>
+					<p {...tickerContentProps}>
+						<b>HINT: </b>
+						{advice}
+					</p>
+				</Tooltip>
+			</Center>
+		</div>
 	);
-};
-
-export default Tickertape;
+}
---
+++ rootine/src/components/Tickertape/tickertapeProps.js
@@ -0,0 +1,28 @@
+export const tickertapeProps = {
+	// margin: "0.5em 0",
+	// minHeight: "2em",
+	width: "100%",
+	isCentered: true,
+};
+
+export const tickerContentProps = {
+	style: {
+		fontSize: "1.1em",
+		// height: "3em",
+		padding: "0.5em 0",
+		margin: "0.8em 0.5em",
+		borderTop: "5px solid var(--green)",
+		borderBottom: "5px solid var(--green)",
+		textAlign: "center",
+		whiteSpace: "nowrap",
+		overflow: "hidden",
+		textOverflow: "ellipsis",
+		minWidth: "100%",
+	},
+};
+
+export const tooltipProps = {
+	backgroundColor: "bisque",
+	color: "var(--black)",
+	fontSize: "lg",
+};
